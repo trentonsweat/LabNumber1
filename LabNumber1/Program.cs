fe6443089851f31f32273ea08983a26dafb55098
trentonsweat <trentonsweat@gmail.com>
2017-09-27 11:11:35 -0400

    First commit!

---
+++ LabNumber1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ProgramContinue = true;
+
+            while (ProgramContinue == true)
+            {
+
+                double Length, Area, Perimeter;
+
+                
+           
+
+
+                
+
+
+
+                string UserChoice;
+                Console.WriteLine("Do you want to run the code again? (N/No/y/yes)");
+                UserChoice = Console.ReadLine();
+                if (UserChoice == "N" || UserChoice == "No")
+                {
+                    ProgramContinue = false;
+                }
+            }
+
+
+
+        }
+    }
+}
